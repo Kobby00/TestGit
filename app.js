5f14f1e3c13112e3cce2feeab7ddd1020ce0b580
Kobby00 <kwabenaoware243@gmail.com>
2024-01-21 12:12:42 +0000

    Changed line 3

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
 //Async functions
-console.log("Start")
+console.log("Start");
+console.log("this is not an easy task");
 
-function loginUser(email,password, callback) {
-    setTimeout(()=>{
-        console.log("Now we have the data");
-        callback ({ userEmail: email,
-                    password: password
-        
-        });
-    }, 3000);
-};
-
-function getUserVideos(email,callback) {
-    setTimeout(()=>{
-        callback(["video1","video2", "video3"])
-    },2000)
+function loginUser(email, password, callback) {
+  setTimeout(() => {
+    console.log("Now we have the data");
+    callback({ userEmail: email, password: password });
+  }, 3000);
 }
 
-const user = loginUser("koo@goomail.com", 12345,(user)=>{
-    console.log(user)
-    getUserVideos(user, (videos)=>{
-        console.log(videos)
-    })
+function getUserVideos(email, callback) {
+  setTimeout(() => {
+    callback(["video1", "video2", "video3"]);
+  }, 2000);
+}
+
+const user = loginUser("koo@goomail.com", 12345, (user) => {
+  console.log(user);
+  getUserVideos(user, (videos) => {
+    console.log(videos);
+  });
 });
 
-console.log('Finish')
-
-
-
+console.log("Finish");
